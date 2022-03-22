8a428bf8d83de4fe0f5e1bfef318b191f45b9c19
Andrew Bish <aebish@mail.fhsu.edu>
2022-03-21 23:08:04 -0700

    added database info

--- config/Database.php
+++ config/Database.php
@@ -1,10 +1,13 @@
 <?php
     class Database {
         //params
-        private $host = 'localhost';
-        private $db_name =  'quotesdb';
-        private $username = 'root';
-        private $password = '';
+        public $url = getenv('JAWSDB_URL');
+        public $dbparts = parse_url($url);
+
+        private $hostname = $dbparts['host'];
+        private $db_name =  ltrim($dbparts['path'],'/');
+        private $username = $dbparts['user'];
+        private $password = $dbparts['pass'];
         private $conn;
         
         //connect db
@@ -12,7 +15,7 @@
             $this->conn = null;
 
             try{
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name,
+                $this->conn = new PDO('mysql:host=' . $this->hostname . ';dbname=' . $this->db_name,
                 $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }catch(PDOException $e) {
